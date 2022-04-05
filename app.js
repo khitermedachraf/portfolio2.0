577f0d9ab3466981610a1e963a06d55e00ada889
KHITER Med Achraf <khiter.med7@gmail.com>
2022-04-05 11:43:03 +0100

    Add the logic to hide menu  after link-click

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 // Implement the logic of the mobile menu
 // the user clicks (or taps) the hamburger button, the mobile menu appears.
 const menuBtn = document.getElementById('menu-btn');
@@ -5,6 +6,8 @@ const header = document.getElementById('header');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeMenu = document.getElementById('close');
 const body = document.getElementById('body');
+const menuLi = document.querySelectorAll('.menu-link');
+body.style.overflow = 'scroll';
 
 menuBtn.addEventListener('click', () => {
   header.classList.add('invisible');
@@ -15,6 +18,11 @@ menuBtn.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
   header.classList.remove('invisible');
   mobileMenu.classList.add('hidden');
-  body.style.overflow = 'auto';
+  body.style.overflow = 'visible';
 });
 
+menuLi.addEventListener('click', () => {
+  header.classList.remove('invisible');
+  mobileMenu.classList.add('hidden');
+  body.style.overflow = 'visible';
+});
