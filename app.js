1b2b41de8ad9e44a24e0d555a6185e9383bbccf8
Christianib003 <christianib003@gmail.com>
2022-04-05 12:06:32 +0200

    Add mobile menu closing function

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 const menuBtn = document.getElementById('menu-btn');
 const header = document.getElementById('header');
 const mobileMenu = document.getElementById('mobile-menu');
+const closeMenu = document.getElementById('close');
+const body = document.getElementById('body');
 
 menuBtn.addEventListener('click', () => {
   header.classList.add('invisible');
   mobileMenu.classList.remove('hidden');
+  body.style.overflow = 'hidden';
 });
+
+closeMenu.addEventListener('click', () => {
+  header.classList.remove('invisible');
+  mobileMenu.classList.add('hidden');
+  body.style.overflow = 'auto';
+});
+
